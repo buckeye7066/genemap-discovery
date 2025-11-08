81b030bf705fae90dac0a47c5baed1ab1150cfde
Dr. John White <buckeye7066@gmail.com>
2025-11-08 04:28:41 +0000

    File changes

--- src/components/ui/progress.jsx
+++ src/components/ui/progress.jsx
@@ -1,10 +1,9 @@
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
 
+import * as React from "react"
 import { cn } from "@/lib/utils"
 
 const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
+  <div
     ref={ref}
     className={cn(
       "relative h-4 w-full overflow-hidden rounded-full bg-slate-100",
@@ -12,12 +11,12 @@ const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
     )}
     {...props}
   >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-blue-600 transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    <div
+      className="h-full bg-blue-600 transition-all duration-300 ease-in-out"
+      style={{ width: `${value || 0}%` }}
     />
-  </ProgressPrimitive.Root>
+  </div>
 ))
-Progress.displayName = ProgressPrimitive.Root.displayName
+Progress.displayName = "Progress"
 
 export { Progress }
